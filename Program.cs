f3559d79a8364ad94afd84ea01c9ce15d3230f9d
steenyweeny <149864766+steenyweeny@users.noreply.github.com>
2026-07-06 13:45:14 +1200

    Remove duplicate console write lines
    Removed duplicate 'Hello, one last change!' lines.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, one last change!");
-Console.WriteLine("Hello, one last change!");
-Console.WriteLine("Hello, one last change!");
-Console.WriteLine("Hello, one last change!");
-Console.WriteLine("Hello, one last change!");
-Console.WriteLine("Hello, one last change!");
